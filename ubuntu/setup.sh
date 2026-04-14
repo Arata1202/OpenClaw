c3226e2f66060d5fc4504d3219a26cc3f3527b57
Arata <aratadayoon@yahoo.co.jp>
2026-04-14 23:23:42 +0900

    fix: add missing Python packages to installation script

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -4,7 +4,7 @@ ARCH="$(dpkg --print-architecture)"
 
 # Ubuntu
 sudo apt update
-sudo apt install -y ca-certificates curl gnupg make build-essential libpng-dev pkg-config nasm
+sudo apt install -y ca-certificates curl gnupg make build-essential libpng-dev pkg-config nasm python3 python3-pip python3-venv ffmpeg
 sudo install -m 0755 -d /etc/apt/keyrings
 
 # Node.js
